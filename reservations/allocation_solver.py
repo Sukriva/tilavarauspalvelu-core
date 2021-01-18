3ccc486d3c91610a5128d7a634bb7d6b61c5dc75
Anttipekka Renfors <anttipekka.renfors@anders.com>
2021-01-18 10:27:08 +0200

    More solver testing.

--- reservations/allocation_solver.py
+++ reservations/allocation_solver.py
@@ -58,32 +58,20 @@ def random_events():
         ))
     return events
 
-class NursesPartialSolutionPrinter(cp_model.CpSolverSolutionCallback):
+class AllocationSolutionsPrinter(cp_model.CpSolverSolutionCallback):
     """Print intermediate solutions."""
 
-    def __init__(self, shifts, num_nurses, num_days, num_shifts, sols):
+    def __init__(self, events, sols):
         cp_model.CpSolverSolutionCallback.__init__(self)
-        self._shifts = shifts
-        self._num_nurses = num_nurses
-        self._num_days = num_days
-        self._num_shifts = num_shifts
+        self._events = events
         self._solutions = set(sols)
         self._solution_count = 0
 
     def on_solution_callback(self):
         if self._solution_count in self._solutions:
             print('Solution %i' % self._solution_count)
-            for d in range(self._num_days):
+            for d in range(self._events):
                 print('Day %i' % d)
-                for n in range(self._num_nurses):
-                    is_working = False
-                    for s in range(self._num_shifts):
-                        if self.Value(self._shifts[(n, d, s)]):
-                            is_working = True
-                            print('  Nurse %i works shift %i' % (n, s))
-                    if not is_working:
-                        print('  Nurse {} does not work'.format(n))
-            print()
         self._solution_count += 1
 
     def solution_count(self):
@@ -109,7 +97,7 @@ class AllocationSolver(object):
             self.model.Add(event.start_date in self.unit.times_open)
 
         solver = cp_model.CpSolver()
-        solver.SearchForAllSolutions(self.model)
+        solver.SearchForAllSolutions(self.model, AllocationSolutionsPrinter(events={}, sols=range(5)))
         print("solver")
 
 
